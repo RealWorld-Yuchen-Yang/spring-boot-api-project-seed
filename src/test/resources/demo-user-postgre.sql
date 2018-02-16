8e837b2132d7b2da28de9311a54e16cb3f6608c6
Yuchen Yang <realworldyucheny@gmail.com>
2018-02-16 21:58:25 +0800

    yyc first commit: add user table mapping and generated code

---
+++ src/test/resources/demo-user-postgre.sql
@@ -0,0 +1,27 @@
+﻿DROP TABLE IF EXISTS "user";
+CREATE TABLE "user"(
+  "id" serial,
+  "username" varchar NOT NULL,
+  "password" varchar NOT NULL,
+  "nick_name" varchar DEFAULT NULL,
+  "sex" integer DEFAULT NULL,
+  "register_date" timestamp NOT NULL,
+  PRIMARY KEY ("id")
+);
+
+select * from "user";
+show tables;
+
+INSERT INTO "user" VALUES ('1', '89921218@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆', '1', '2017-06-23 14:24:23');
+INSERT INTO "user" VALUES ('2', '2@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-2', '1', '2017-06-23 14:24:23');
+INSERT INTO "user" VALUES ('3', '3@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-3', '1', '2017-06-23 14:24:23');
+INSERT INTO "user" VALUES ('4', '4@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-4', '1', '2017-06-23 14:24:23');
+INSERT INTO "user" VALUES ('5', '5@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-5', '1', '2017-06-23 14:24:23');
+INSERT INTO "user" VALUES ('6', '6@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-6', '1', '2017-06-23 14:24:23');
+INSERT INTO "user" VALUES ('7', '7@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-7', '1', '2017-06-23 14:24:23');
+INSERT INTO "user" VALUES ('8', '8@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-8', '1', '2017-06-23 14:24:23');
+INSERT INTO "user" VALUES ('9', '9@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-9', '1', '2017-06-23 14:24:23');
+INSERT INTO "user" VALUES ('10', '10@qq.com', '1ee04e0b1cb5af7367c80c22e42efd8b', '土豆-10', '1', '2017-06-23 14:24:23');
+
+
+
